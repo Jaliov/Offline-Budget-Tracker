86aa0d0bca4f40924df31745286b3fe075d2b3ec
Joel Rudin <jarud55@gmail.com>
2020-03-29 10:07:32 -0400

    updates

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+//router
 router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
